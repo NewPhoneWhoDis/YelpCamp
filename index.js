7bba36c05c8575c085cab34c2e0e8f9f5bb9985d
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-04-04 23:46:55 +0200

    added review option

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const {campgroundSchema} = require('./validationSchemas');
+const Review = require('./models/review');
 
 
 
@@ -70,6 +71,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
